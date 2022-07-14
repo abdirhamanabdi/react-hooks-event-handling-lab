1da8f42cfc0698dc02f25deb4eb6c0cb3ade024e
AbdirahmanAbdi <abdirahmanabdi.ma@gmail.com>
2022-07-14 07:38:48 +0300

    your message

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,11 @@
 // Code EyesOnMe Component Here
+export default function () {
+  return (
+    <button
+      onFocus={(e) => console.log("Good!")}
+      onBlur={(e) => console.log("Hey! Eyes on me!")}
+    >
+      Eyes on me
+    </button>
+  );
+}
