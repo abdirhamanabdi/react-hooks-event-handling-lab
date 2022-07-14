f017bccfb381b4a3bcdcf17b3cd56ba6e2337135
AbdirahmanAbdi <abdirahmanabdi.ma@gmail.com>
2022-07-14 07:38:46 +0300

    commit tree 1a0ffb0579299752b56bcae34b549eb64c0339b6

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,9 @@
 // Code Keypad Component Here
+export default function Keypad(){
+
+    return(
+    <input type="password" id="keypad" 
+    onChange={(e) => console.log("Entering password...")}
+    />
+    )
+}
